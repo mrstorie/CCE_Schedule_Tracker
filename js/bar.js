17300c7f50c20fdde8a14700de9ac9f93d68bd24
hooperwf1 <whooper@groglinux.com>
2022-01-18 20:17:23 -0700

    Make the bar move smoothly over a given time period

--- js/bar.js
+++ js/bar.js
@@ -1,14 +1,41 @@
-function move(){
-    var bar = document.getElementById("bar");
-    updateBar(bar, 0);
-}
-
-// Progress is a decimal between 0 and 1
-function updateBar(bar, progress) {
-    if(progress > 1){
-        return;
+class ProgressBar {
+    constructor(startTime, goalTime, barId){
+        this.startTime = startTime;
+        this.goalTime = goalTime;
+        this.bar = document.getElementById(barId);
     }
 
-    bar.style.width = (progress * 100) + "%"; 
-    setTimeout(updateBar, 50, bar, progress + 0.01);
+    setStartTime(startTime){
+        this.startTime = startTime;
+    }
+
+    startMoving(){
+        const fps = 1000 / 30;
+        this.interval = setInterval(ProgressBar.updateBar, fps, this);
+    }
+
+    endOfInterval(){
+        clearInterval(this.interval);
+    }
+
+    // Convert the dates into miliseconds, then get a percentage completion
+    static updateBar(bar) {
+        var start = bar.startTime.getTime();
+        var end = bar.goalTime.getTime();
+        var length = end - start;
+        var elapsed = Date.now() - start;
+
+        bar.bar.style.width = (elapsed/length * 100) + "%"; 
+
+        if(elapsed/length >= 1){
+            bar.endOfInterval();
+        }
+    }
+}
+
+// Show the bar moving over a one minute period
+function testBar(){
+    const oneMinute = 1000 * 60;
+    var bar = new ProgressBar(new Date(), new Date(Date.now() + oneMinute), "bar");
+    bar.startMoving();
 }

148419450d37e65156cd4d9a0c33e39db62f7da8
hooperwf1 <whooper@groglinux.com>
2022-04-21 14:20:29 -0600

    Auto setup

---
+++ tools/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd /home/schedule
+mkdir -p .config/autostart
+
+# Download files
+/usr/bin/curl https://hooperwf1.gitub.io/MVHS_Schedule_Tracker/tools/kiosk.sh | /usr/bin/tee kiosk.sh
+/usr/bin/curl https://hooperwf1.gitub.io/MVHS_Schedule_Tracker/tools/kiosk.desktop | /usr/bin/tee .config/autostart/kiosk.desktop
+
+# Enable egl for chromium
+/usr/bin/echo 'export CHROMIUM_FLAGS="$CHROMIUM_FLAGS --use-gl=egl"' | /usr/bin/sudo /usr/bin/tee /etc/chromium.d/egl

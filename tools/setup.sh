ad029e83cb08a954e6d5807f9ef82222ebd5408b
hooperwf1 <whooper@groglinux.com>
2022-04-21 14:27:14 -0600

    Make kiosk.sh executeable

--- tools/setup.sh
+++ tools/setup.sh
@@ -7,6 +7,7 @@ mkdir -p .config/autostart
 /usr/bin/curl https://hooperwf1.gitub.io/MVHS_Schedule_Tracker/tools/kiosk.sh | /usr/bin/tee kiosk.sh
 /usr/bin/curl https://hooperwf1.gitub.io/MVHS_Schedule_Tracker/tools/kiosk.desktop | /usr/bin/tee .config/autostart/kiosk.desktop
 sudo apt install unclutter
+/usr/bin/chmod a+x kiosk.sh
 
 # Enable egl for chromium
 /usr/bin/echo 'export CHROMIUM_FLAGS="$CHROMIUM_FLAGS --use-gl=egl"' | /usr/bin/sudo /usr/bin/tee /etc/chromium.d/egl

58094d3f038d1072d8638206ca18635b470c0a8f
hooperwf1 <whooper@groglinux.com>
2022-04-21 14:26:07 -0600

    Install unclutter

--- tools/setup.sh
+++ tools/setup.sh
@@ -6,6 +6,7 @@ mkdir -p .config/autostart
 # Download files
 /usr/bin/curl https://hooperwf1.gitub.io/MVHS_Schedule_Tracker/tools/kiosk.sh | /usr/bin/tee kiosk.sh
 /usr/bin/curl https://hooperwf1.gitub.io/MVHS_Schedule_Tracker/tools/kiosk.desktop | /usr/bin/tee .config/autostart/kiosk.desktop
+sudo apt install unclutter
 
 # Enable egl for chromium
 /usr/bin/echo 'export CHROMIUM_FLAGS="$CHROMIUM_FLAGS --use-gl=egl"' | /usr/bin/sudo /usr/bin/tee /etc/chromium.d/egl

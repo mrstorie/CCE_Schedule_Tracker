53e336f4ba986f2805be7096bf5d6c9acb278481
hooperwf1 <whooper@groglinux.com>
2022-04-26 13:15:48 -0600

    Fix setup.sh

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-cd /home/schedule
-mkdir -p .config/autostart
+mkdir -p $HOME/.config/autostart
 
 # Download files
-/usr/bin/curl https://hooperwf1.gitub.io/MVHS_Schedule_Tracker/tools/kiosk.sh | /usr/bin/tee kiosk.sh
-/usr/bin/curl https://hooperwf1.gitub.io/MVHS_Schedule_Tracker/tools/kiosk.desktop | /usr/bin/tee .config/autostart/kiosk.desktop
+/usr/bin/wget https://hooperwf1.gitub.io/MVHS_Schedule_Tracker/tools/kiosk.sh -O $HOME/kiosk.sh
+/usr/bin/wget https://hooperwf1.gitub.io/MVHS_Schedule_Tracker/tools/kiosk.desktop -O $HOME/.config/autostart/kiosk.desktop
 sudo apt install unclutter
-/usr/bin/chmod a+x kiosk.sh
+/usr/bin/chmod a+x $HOME/kiosk.sh
 
 # Enable egl for chromium
 /usr/bin/echo 'export CHROMIUM_FLAGS="$CHROMIUM_FLAGS --use-gl=egl"' | /usr/bin/sudo /usr/bin/tee /etc/chromium.d/egl
+
+echo Please reboot or run kiosk.sh to start kiosk.

ef3b0f6144e7020afadd64a69682b7782fd478c3
CSMVHS <104864350+CSMVHS@users.noreply.github.com>
2022-05-06 13:27:44 -0600

    Update kiosk.sh

--- tools/kiosk.sh
+++ tools/kiosk.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 unclutter -idle 0 &
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk hooperwf1.github.io/MVHS_Schedule_Tracker
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk csmvhs.github.io/MVHS_Schedule_Tracker

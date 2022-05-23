62d2d8f6b5f1b67d6deec62084db508208c06eca
CSMVHS <104864350+CSMVHS@users.noreply.github.com>
2022-05-22 20:05:28 -0600

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -182,6 +182,18 @@ function dateSchedule(){
 
     switch(weekDay){
 	case 1://Monday
+        schedule[0] = new Schedule("7:34;SOAR;9:05,9:06;Passing Period;9:09,9:10;Period 7;10:40,10:41;Passing Period;10:44,10:45;Period 6;12:15")
+        break;
+	case 2://Tuesday
+        schedule[0] = new Schedule("7:34;Period 5;9:05,9:06;Passing Period;9:09,9:10;Period 4;10:40,10:41;Passing Period;10:44,10:45;Period 3;12:15")
+        break;
+
+	case 3://Wednesday
+        schedule[0] = new Schedule("7:34;SOAR;9:05,9:06;Passing Period;9:09,9:10;Period 2;10:40,10:41;Passing Period;10:44,10:45;Period 1;12:15")
+        break;
+		    
+	/*
+	case 1://Monday
 	case 2://Tuesday
 	case 5://Friday
 		console.log("Odd Block");
@@ -198,6 +210,7 @@ function dateSchedule(){
 		schedule[0] = new Schedule("7:34;SOAR;8:55,8:55;Passing Period;9:00,9:00;Advisement;9:25,9:25;Passing Period;9:30,9:30;Period 2;11:02,11:02;A Lunch;11:36,11:36;Passing Period;11:41,11:41;Period 4;13:13,13:13;Passing Period;13:18,13:18;Period 6;14:50")
 		schedule[1] = new Schedule("11:02;Passing Period;11:07,11:07;Period 4;12:39,12:39;B Lunch;13:13");
 		break;
+	*/
     }
 	//console.log("Normal");
 		//schedule[0] = new Schedule("7:34;Period 1;8:28,8:28;Passing Period;8:32,8:32;Period 2;9:26,9:26;Passing Period;9:30,9:30;Period 3;10:24,10:24;Passing Period;10:28,10:28;Period 4;11:22,11:22;A Lunch;11:56,11:56;Passing Period;12:00,12:00;Period 5;12:54,12:54;Passing Period;12:58,12:58;Period 6;13:52,13:52;Passing Period;13:56,13:56;Period 7;14:50");

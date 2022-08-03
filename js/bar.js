fdb0634302afd39a4d3d8666cb8dcce2e7dbf9af
CSMVHS <104864350+CSMVHS@users.noreply.github.com>
2022-08-03 15:20:47 -0600

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -196,21 +196,21 @@ function dateSchedule(){
 	break;
 	*/
 		    
-	Original
+	//Original
 	case 1://Monday
 	case 2://Tuesday
 	case 5://Friday
-		console.log("Odd Block");
+		//console.log("Odd Block");
 		schedule[0] = new Schedule("7:53;Period 1;9:25,9:25;Passing Period;9:30,9:30;Period 3;11:02,11:02;A Lunch;11:36,11:36;Passing Period;11:41,11:41;Period 5;13:13,13:13;Passing Period;13:18,13:18;Period 7;14:50");
 		schedule[1] = new Schedule("11:02;Passing Period;11:07,11:07;Period 5;12:39,12:39;B Lunch;13:13");
 		break;
 	case 3://Wednesday
-		console.log("Odd Block");
+		//console.log("Odd Block");
 		schedule[0] = new Schedule("7:53;Period 1;9:25,9:25;Passing Period;9:30,9:30;Period 3;11:02,11:02;A Lunch;11:36,11:36;Passing Period;11:41,11:41;Period 5;13:13,13:13;Passing Period;13:18,13:18;Period 7;14:50");
 		schedule[1] = new Schedule("11:02;Passing Period;11:07,11:07;Period 5;12:39,12:39;B Lunch;13:13");
 		break;
 	case 4://Thursday
-		console.log("Even Block");
+		//console.log("Even Block");
 		schedule[0] = new Schedule("7:34;SOAR;8:55,8:55;Passing Period;9:00,9:00;Advisement;9:25,9:25;Passing Period;9:30,9:30;Period 2;11:02,11:02;A Lunch;11:36,11:36;Passing Period;11:41,11:41;Period 4;13:13,13:13;Passing Period;13:18,13:18;Period 6;14:50")
 		schedule[1] = new Schedule("11:02;Passing Period;11:07,11:07;Period 4;12:39,12:39;B Lunch;13:13");
 		break;

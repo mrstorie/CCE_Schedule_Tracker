c6a3021b2e8a4ea06041f820824758748615a29f
CSMVHS <104864350+CSMVHS@users.noreply.github.com>
2022-08-18 13:08:56 -0600

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -214,7 +214,7 @@ case 3://Wednesday
         break;
 
 case 4://Thursday
-        schedule[0] = new Schedule("7:30;Teacher PLC;8:00,8:00;Period 2;9:30,9:30;Passing Period;9:35,9:35;EAGLE TIME;11:05,11:05;A Lunch;11:40,11:40;Passing Period;11:45,11:45;Period 5;13:15,13:15;Passing Period;13:20,13:20;Period 7;14:50")
+        schedule[0] = new Schedule("7:30;Teacher PLC;8:00,8:00;Period 2;9:30,9:30;Passing Period;9:35,9:35;EAGLE TIME;11:05,11:05;A Lunch;11:40,11:40;Passing Period;11:45,11:45;Period 4;13:15,13:15;Passing Period;13:20,13:20;Period 6;14:50")
         schedule[1] = new Schedule("11:05;Passing Period;11:10,11:10;Period 4;12:40,12:40;B Lunch;13:15");
         break;
     

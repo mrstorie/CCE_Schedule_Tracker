ab735d393015c17aaaddfe37b4c0046b9d9b3334
CSMVHS <104864350+CSMVHS@users.noreply.github.com>
2022-10-09 11:48:01 -0500

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -235,19 +235,18 @@ case 2://Tuesday
         break;
 
 case 3://Wednesday
-        schedule[0] = new Schedule("7:30;Teacher Office Hours;7:45,7:45;Period 1;8:36,8:36;Passing Period;8:41,8:41;Period 2;9:32,9:32;Passing Period;9:37,9:37;Period 3;10:28,10:28;Passing Period;10:33,10:33;Period 4;11:24,11:24;A Lunch;12:02,12:02;Passing Period;12:07,12:07;Period 5;12:58,12:58;Passing Period;13:03,13:03;Period 6;13:54,13:54;Passing Period;13:59,13:59;Period 7;14:50");
-        schedule[1] = new Schedule("11:24;Passing Period;11:29,11:29;Period 5;12:20,12:20;B Lunch;12:58");
+        schedule[0] = new Schedule("7:15;Testing;11:30,11:30;Lunch;12:16,12:16;Passing Period;12:21,12:21;Period 6;13:33,13:33;Passing Period;13:38,13:38;Period 7;14:50,");
         break;
 
 case 4://Thursday
-        schedule[0] = new Schedule("7:30;Teacher PLC;8:00,8:00;Period 1;9:30,9:30;Passing Period;9:35,9:35;Period 3;11:05,11:05;A-Taste of Vista;11:40,11:40;Passing Period;11:45,11:45;Period 5;13:15,13:15;Passing Period;13:20,13:20;Period 7;14:50");
-        schedule[1] = new Schedule("11:05;Passing Period;11:10,11:10;Period 5;12:40,12:40;B-Taste of Vista;13:15");
+        schedule[0] = new Schedule("7:45;Period 1;8:57,8:57;Passing Period;9:02,9:02;Period 2;10:14,10:14;Passing Period;10:19,10:19;Period 3;11:31,11:31;A Lunch;12:16,12:16;Passing Period;12:21,12:21;Period 4;13:33,13:33;Passing Period;13:38,13:38;Period 5;14:50");
+        schedule[1] = new Schedule("11:31;Passing Period;11:36,11:36;Period 4;12:48,12:48;B Lunch;13:33");
         break;
     
 case 5://Friday
-        schedule[0] = new Schedule("7:30;Teacher PLC;8:00,8:00;Period 2;9:30,9:30;Passing Period;9:35,9:35;Homecoming Assembly;11:05,11:05;A Lunch;11:40,11:40;Passing Period;11:45,11:45;Period 4;13:15,13:15;Passing Period;13:20,13:20;Period 6;14:50")
-        schedule[1] = new Schedule("11:05;Passing Period;11:10,11:10;Period 4;12:40,12:40;B Lunch;13:15");
-        break;
+        schedule[0] = new Schedule("7:30;Teacher Office Hours;7:45,7:45;Period 1;8:36,8:36;Passing Period;8:41,8:41;Period 2;9:32,9:32;Passing Period;9:37,9:37;Period 3;10:28,10:28;Passing Period;10:33,10:33;Period 4;11:24,11:24;A Lunch;12:02,12:02;Passing Period;12:07,12:07;Period 5;12:58,12:58;Passing Period;13:03,13:03;Period 6;13:54,13:54;Passing Period;13:59,13:59;Period 7 test;14:50");
+	schedule[1] = new Schedule("11:24;Passing Period;11:29,11:29;Period 5;12:20,12:20;B Lunch;12:58");
+	break;
     }
 	//console.log("Normal");
 		//schedule[0] = new Schedule("7:30;Teacher Office Hours;7:45, 7:45;Period 1;8:36, 8:36;Passing Period;8:41, 8:41;Period 2;9:32, 9:32;Passing Period;9:37, 9:37;Period 3;10:28, 10:28;Passing Period;10:33, 10:33;Period 4;11:24, 11:24; A Lunch; 12:02, 12:02;Passing Period;12:07, 12:07;Period 5;12:58, 12:58; Passing Period; 1:03, 1:03; Period 6;1:54, 1:54; Passing Period; 1:59, 1:59;Period 7;2:50");  

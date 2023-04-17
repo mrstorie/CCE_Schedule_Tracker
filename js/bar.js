a8309ef113dc0e506616daf57e9c693f1eed3dfd
CSMVHS <104864350+CSMVHS@users.noreply.github.com>
2023-04-17 07:37:01 -0600

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -183,7 +183,7 @@ function dateSchedule(){
 	
 		
 //************************22 bell schedule**************
-/*
+
 	case 1://Monday
 		schedule[0] = new Schedule("7:30;Teacher Office Hours;7:45,7:45;Period 1;8:36,8:36;Passing Period;8:41,8:41;Period 2;9:32,9:32;Passing Period;9:37,9:37;Period 3;10:28,10:28;Passing Period;10:33,10:33;Period 4;11:24,11:24;A Lunch;12:02,12:02;Passing Period;12:07,12:07;Period 5;12:58,12:58;Passing Period;13:03,13:03;Period 6;13:54,13:54;Passing Period;13:59,13:59;Period 7;14:50");
 		schedule[1] = new Schedule("11:24;Passing Period;11:29,11:29;Period 5;12:20,12:20;B Lunch;12:58");
@@ -209,9 +209,9 @@ function dateSchedule(){
 		break;
 
 		}
-		
+/*		
 /////Testing week
-*/		    
+	    
 	    
 	case 1://Monday
 		schedule[0] = new Schedule("7:30;Teacher Office Hours;7:45,7:45;Period 1;8:36,8:36;Passing Period;8:41,8:41;Period 2;9:32,9:32;Passing Period;9:37,9:37;Period 3;10:28,10:28;Passing Period;10:33,10:33;Period 4;11:24,11:24;A Lunch;12:02,12:02;Passing Period;12:07,12:07;Period 5;12:58,12:58;Passing Period;13:03,13:03;Period 6;13:54,13:54;Passing Period;13:59,13:59;Period 7;14:50");

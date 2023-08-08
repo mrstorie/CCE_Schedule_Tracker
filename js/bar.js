d8e6abb364334baf0831cf86b77253b05e6e7b92
CSMVHS <104864350+CSMVHS@users.noreply.github.com>
2023-08-08 09:42:00 -0600

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -182,7 +182,7 @@ function dateSchedule(){
     switch(weekDay){
 
 	case 1://Monday
-		schedule[0] = new Schedule("7:30;Teacher Office Hours;7:45,7:45;Period 1;9:19,9:19;Passing Period;9:24,9:24;Period 2;10:58,10:58;A Lunch;11:32,11:32;Passing Period;11:37,11:37;Period 3;13:11,13:11;Passing Period;13:16,13:16;Period 4;14:50);
+		schedule[0] = new Schedule("7:30;Teacher Office Hours;7:45,7:45;Period 1;9:19,9:19;Passing Period;9:24,9:24;Period 2;10:58,10:58;A Lunch;11:32,11:32;Passing Period;11:37,11:37;Period 3;13:11,13:11;Passing Period;13:16,13:16;Period 4;14:50");
 		schedule[1] = new Schedule("10:58;Passing Period;11:03,11:03;Period 3;12:37,12:37;B Lunch;13:11");
 		break;
 	case 2://Tuesday
@@ -191,7 +191,7 @@ function dateSchedule(){
 		break;
 
 	case 3://Wednesday
-		schedule[0] = new Schedule("7:30;Teacher Office Hours;7:45,7:45;Period 1;9:19,9:19;Passing Period;9:24,9:24;Period 2;10:58,10:58;A Lunch;11:32,11:32;Passing Period;11:37,11:37;Period 3;13:11,13:11;Passing Period;13:16,13:16;Period 4;14:50);
+		schedule[0] = new Schedule("7:30;Teacher Office Hours;7:45,7:45;Period 1;9:19,9:19;Passing Period;9:24,9:24;Period 2;10:58,10:58;A Lunch;11:32,11:32;Passing Period;11:37,11:37;Period 3;13:11,13:11;Passing Period;13:16,13:16;Period 4;14:50");
 		schedule[1] = new Schedule("10:58;Passing Period;11:03,11:03;Period 3;12:37,12:37;B Lunch;13:11");
 		break;
 		    

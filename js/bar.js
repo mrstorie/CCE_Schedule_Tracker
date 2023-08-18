2d795bf8e9d0e6c5d83b80eabaf47816f12aa57d
CSMVHS <104864350+CSMVHS@users.noreply.github.com>
2023-08-18 14:56:31 -0600

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -269,27 +269,7 @@ function dateSchedule(){
  /* 		
     }
 
-	case 1://Monday
-		schedule[0] = new Schedule("7:30;Teacher Office Hours;7:45,7:45;Period 1;8:36,8:36;Passing Period;8:41,8:41;Period 2;9:32,9:32;Passing Period;9:37,9:37;Period 3;10:28,10:28;Passing Period;10:33,10:33;Period 4;11:24,11:24;A Lunch;12:02,12:02;Passing Period;12:07,12:07;Period 5;12:58,12:58;Passing Period;13:03,13:03;Period 6;13:54,13:54;Passing Period;13:59,13:59;Period 7;14:50");
-		schedule[1] = new Schedule("11:24;Passing Period;11:29,11:29;Period 5;12:20,12:20;B Lunch;12:58");
-		break;
-	case 2://Tuesday
-		schedule[0] = new Schedule("7:30;Teacher Office Hours;7:45,7:45;Period 1;8:36,8:36;Passing Period;8:41,8:41;Period 2;9:32,9:32;Passing Period;9:37,9:37;Period 3;10:28,10:28;Passing Period;10:33,10:33;Period 4;11:24,11:24;A Lunch;12:02,12:02;Passing Period;12:07,12:07;Period 5;12:58,12:58;Passing Period;13:03,13:03;Period 6;13:54,13:54;Passing Period;13:59,13:59;Period 7;14:50");
-		schedule[1] = new Schedule("11:24;Passing Period;11:29,11:29;Period 5;12:20,12:20;B Lunch;12:58");
-		break;
-	case 3://Wednesday
-		schedule[0] = new Schedule("7:30;Teacher PLC;8:00,8:00;Period 1;9:30,9:30;Passing Period;9:35,9:35;Period 3;11:05,11:05;A Lunch;11:40,11:40;Passing Period;11:45,11:45;Period 5;13:15,13:15;Passing Period;13:20,13:20;Period 7;14:50");
-		schedule[1] = new Schedule("11:05;Passing Period;11:10,11:10;Period 5;12:40,12:40;B Lunch;13:15");
-		break;
-	case 4://Thursday
-		schedule[0] = new Schedule("7:30;Teacher PLC;8:00,8:00;Period 2;9:30,9:30;Passing Period;9:35,9:35;Advisement;10:05,10:05;EAGLE TIME;11:05,11:05;A Lunch;11:40,11:40;Passing Period;11:45,11:45;Period 4;13:15,13:15;Passing Period;13:20,13:20;Period 6;14:50")
-		schedule[1] = new Schedule("11:05;Passing Period;11:10,11:10;Period 4;12:40,12:40;B Lunch;13:15");
-		break;
-   
-	case 5://Friday
-		schedule[0] = new Schedule("7:30;Teacher Office Hours;7:45,7:45;Period 1;8:36,8:36;Passing Period;8:41,8:41;Period 2;9:32,9:32;Passing Period;9:37,9:37;Period 3;10:28,10:28;Passing Period;10:33,10:33;Period 4;11:24,11:24;A Lunch;12:02,12:02;Passing Period;12:07,12:07;Period 5;12:58,12:58;Passing Period;13:03,13:03;Period 6;13:54,13:54;Passing Period;13:59,13:59;Period 7 test;14:50");
-		schedule[1] = new Schedule("11:45;Passing Period;11:50,11:50;Period 5;12:35,12:35;B Lunch;13:10");
-		break;
+
   	    
 		    
 		    

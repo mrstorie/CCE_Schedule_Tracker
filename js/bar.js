7e032436184494cbe44410b26fc0046bbeb76418
CSMVHS <104864350+CSMVHS@users.noreply.github.com>
2024-03-01 08:35:59 -0700

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -205,32 +205,6 @@ function dateSchedule(){
 		schedule[0] = new Schedule("7:00;Good Morning!;7:30,7:30;Teacher Office Hours;7:45,7:45;Period 1;8:36,8:36;Passing Period;8:41,8:41;Period 2;9:32,9:32;Passing Period;9:37,9:37;Period 3;10:28,10:28;Passing Period;10:33,10:33;Period 4;11:24,11:24;A Lunch;12:02,12:02;Passing Period;12:07,12:07;Period 5;12:58,12:58;Passing Period;13:03,13:03;Period 6;13:54, 13:54;Passing Period;13:59,13:59;Period 7;14:50");
 		schedule[1] = new Schedule("11:24;Passing Period;11:29,11:29;Period 5;12:20,12:20;B Lunch;12:58");   
         	break;
-
-/*
-case 1://Monday
-		schedule[0] = new Schedule("7:00;Good Morning!;7:30,7:30;Teacher Office Hours;7:45,7:45;Period 1;9:10,9:10;Passing Period;9:15,9:15;Period 2;10:40,10:40;Passing Period;10:45,10:45;Kick-off Assembly;11:25,11:25;A Lunch;11:50,11:50;Passing Period;11:55,11:55;Period 3;13:20,13:20;Passing Period;13:25,13:25;Period 4;14:50");
-		schedule[1] = new Schedule("11:25;Passing Period;11:30,11:30;Period 3;12:55,12:55;B Lunch;13:20");
-		break;
-		    
-   	 case 2://Tuesday
-		schedule[0] = new Schedule("7:00;Good Morning!;7:30,7:30;Teacher PLC;8:10,8:10;Period 5;9:44,9:44;Homeroom;9:54,9:54;S.A.S;10:54,10:54;A Lunch;11:32,11:32;Passing Period;11:37,11:37;Period 6;13:11,13:11;Passing Period;13:16,13:16;Period 7;14:50");
-		schedule[1] = new Schedule("10:54;Passing Period;10:59,10:59;Period 6;12:33,12:33;B Lunch;13:11");
-		break;
-	case 3://Wednesday
-		schedule[0] = new Schedule("7:00;Good Morning!;7:30,7:30;Teacher Office Hours;7:45,7:45;Period 1;9:19,9:19;Passing Period;9:24,9:24;Period 2;10:58,10:58;A Lunch;11:32,11:32;Passing Period;11:37,11:37;Period 3;13:11,13:11;Passing Period;13:16,13:16;Period 4;14:50");
-		schedule[1] = new Schedule("10:58;Passing Period;11:03,11:03;Period 3;12:37,12:37;B Lunch;13:11");
-		break;
-		    
-	case 4://Thursday
-		schedule[0] = new Schedule("7:00;Good Morning!;7:30,7:30;Teacher PLC;8:10,8:10;Period 5;9:44,9:44;Homeroom;9:54,9:54;Eagle Time;10:54,10:54;A Lunch;11:32,11:32;Passing Period;11:37,11:37;Period 6;13:11,13:11;Passing Period;13:16,13:16;Period 7;14:50");
-		schedule[1] = new Schedule("10:54;Passing Period;10:59,10:59;Period 6;12:33,12:33;B Lunch;13:11");
-		break;
-		    
-	case 5://Friday
-		schedule[0] = new Schedule("7:00;Good Morning!;7:30,7:30;Teacher Office Hours;7:45,7:45;Period 1;8:25,8:25;Passing Period;8:30,8:30;Period 2;9:10,9:10;Passing Period;9:15,9:15;Period 3;9:55,10:00;Wish Assembly!!;11:20,11:20;Passing Period;11:25,11:25;Period 4;12:05,12:05;A Lunch;12:35,12:35;Passing Period;12:40,12:40;Period 5;13:20,13:20;Passing Period;13:25,13:25;Period 6;14:05, 14:05;Passing Period;14:10,14:10;Period 7;14:50");
-		schedule[1] = new Schedule("12:05;Passing Period;12:10,12:10;Period 5;12:50,12:50;B Lunch;13:20");
-        	break;*/
-
 		    
     }
 	    

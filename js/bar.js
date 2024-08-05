7fd7e71b7a46f32494ee69f1211043e24f218350
CSMVHS <104864350+CSMVHS@users.noreply.github.com>
2024-08-05 09:43:52 -0600

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -225,8 +225,8 @@ function dateSchedule(){
 		break;
 		    
 	case 4://Thursday
-		schedule[0] = new Schedule("7:00;Good Luck on Finals!;7:30,7:30;Teacher Office Hours;7:45,7:45;Homeroom/Eagle Time;9:15,9:15;Passing Period;9:20,9:20;Period 7 Final;11:20,11:20;Passing Period;11:25,11:25;Make-up Final;14:50");
-		schedule[1] = new Schedule("12:30;Break;13:00");  
+		schedule[0] = new Schedule("7:00;Good Morning!;7:30,7:30;Teacher Office Hours;7:45,7:45;Period 1;8:36,8:36;Passing Period;8:41,8:41;Period 2;9:32,9:32;Passing Period;9:37,9:37;Period 3;10:28,10:28;Passing Period;10:33,10:33;Period 4;11:24,11:24;A Lunch;12:02,12:02;Passing Period;12:07,12:07;Period 5;12:58,12:58;Passing Period;13:03,13:03;Period 6;13:54, 13:54;Passing Period;13:59,13:59;Period 7;14:50");		
+	        schedule[1] = new Schedule("12:05;Passing Period;12:10,12:10;Period 5;12:50,12:50;B Lunch;13:20");  
 		break;
 		    
 	case 5://Friday

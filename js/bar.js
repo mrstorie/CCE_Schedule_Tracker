c247a7f21537634cf9200f78d0c688a976d83a03
mrstorie <mrstorie@gmail.com>
2025-04-23 14:33:44 -0600

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -226,7 +226,7 @@ function dateSchedule() {
     switch (weekDay) {
         case 1: //Monday
            schedule[0] = new Schedule(
-               "7:00;Good Morning!;7:30,7:30;Teacher Office Hours;7:45,7:45;Period 1;9:19,9:19;Passing Period;9:24,9:24;Period 2;10:58,10:58;A Lunch;11:32,11:32;Passing Period;11:37,11:37;Period 3;13:11,13:11;Passing Period;13:16,13:16;Period 4;14:50",
+               "7:00;Good Morning!;8:30,8:30;Teacher Office Hours;8:45,8:45;Period 1;9:19,9:19;Passing Period;9:24,9:24;Period 2;10:58,10:58;A Lunch;11:32,11:32;Passing Period;11:37,11:37;Period 3;13:11,13:11;Passing Period;13:16,13:16;Period 4;14:50",
            );
            break;
 
